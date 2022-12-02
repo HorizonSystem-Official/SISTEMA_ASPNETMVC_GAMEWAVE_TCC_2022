acb848da06630a744afd54632240295fef32cc46
GuiMatheus1313 <82453465+GuiMatheus1313@users.noreply.github.com>
2022-12-01 21:43:57 -0300

    Adição da classe authorize para login e layout shared acesso restrito (Nada programado ainda)

---
+++ Utils/CustomAuthorize.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TCC_Sistema_Cliente_Jogos_2022.Utils
+{
+    public class CustomAuthorize
+    {
+    }
+}
